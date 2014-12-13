99ac5b7610913d6ce21d4733701d1adabec5fd87
siddiqui00 <arbaz00@gmail.com>
2014-12-13 22:58:48 +0530

    Stop Words
    Lets you take two inputs one containing Stop Words and other containing
    the search query and generates another text file with all the unique
    words.

---
+++ src/StopWords.java
@@ -0,0 +1,180 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.io.*;
+
+public class StopWords 
+{
+	
+	
+	public static ArrayList LoadStopWords()
+	{
+		List<String> StopWords = new ArrayList<String>();
+		int count=0;
+		BufferedReader in = null;
+		try {   
+		    in = new BufferedReader(new FileReader("C:\\Users\\siddiqui\\workspace\\SEO\\Files\\MYSTWORD.txt"));
+		    String str;
+		    while ((str = in.readLine()) != null) {
+		        StopWords.add(str);
+		        count++; }
+		    //System.out.println(count);
+		} catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		finally
+		{
+		    try
+		    {
+		        if ( in != null)
+		        in.close( );
+		    }
+		    catch ( IOException e)
+		    {
+		    }
+		}
+		return (ArrayList) StopWords;
+		
+	}
+	
+	
+	
+	public static ArrayList LoadFile()
+	{
+		BufferedReader in = null;
+		String line = new String();
+		int count=0,i;
+		String[] words = new String[1000];
+		List<String> fileword = new ArrayList<String>();
+		//String fileword = new String("");
+		try {
+			 in = new BufferedReader(new FileReader("C:\\Users\\siddiqui\\workspace\\SEO\\Files\\sportsnews.txt"));
+			 while ((line = in.readLine()) != null) {
+	                words = line.split(" ");
+	                for(i=0; i<words.length; i++ )
+	                {
+	                	//fileword += (words[i] + " ");
+	                	fileword.add(words[i]);
+	                	count++;
+	                }
+			 }
+			// System.out.println(count);
+		}
+		catch (FileNotFoundException e) {
+		    e.printStackTrace();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		finally
+		{
+		    try
+		    {
+		        if ( in != null)
+		        in.close( );
+		    }
+		    catch ( IOException e)
+		    {
+		    }
+		}
+		return (ArrayList) fileword;
+		
+	}
+	
+	
+	
+	
+	
+	public static String Compare()
+	{
+		int i,j,count=0;
+		List<String> StopWords = new ArrayList<String>();
+		List<String> fileword = new ArrayList<String>();
+		List<String> FinalList = new ArrayList<String>();
+		StopWords=LoadStopWords();
+		fileword=LoadFile();
+		String Output="";
+		/*
+		for(i=0; i<fileword.size(); i++ )
+		{
+			for(j=0; j<StopWords.size();j++)
+			{
+				if(StopWords.get(j).equals(fileword.get(i)))
+				{
+					FinalList.add(fileword.get(i)) ;
+					count++;
+				}
+			}
+		}
+		*/
+		fileword.removeAll( StopWords );
+		for (String s : fileword)
+		{
+		    Output += s + " ";
+		}
+		System.out.println(Output);
+		//System.out.println(a);
+		//System.out.println(count);
+		return Output;
+		
+	}
+	
+	
+	
+	
+	public static void GenarateFile()
+	{
+		String Output=Compare();
+		BufferedWriter writer = null;
+		try
+		{
+		    writer = new BufferedWriter( new FileWriter("C:\\Users\\siddiqui\\workspace\\SEO\\Output\\nsportsnews.txt"));
+		    writer.write(Output);
+
+		}
+		catch ( IOException e)
+		{
+		}
+		finally
+		{
+		    try
+		    {
+		        if ( writer != null)
+		        writer.close( );
+		    }
+		    catch ( IOException e)
+		    {
+		    }
+		}
+	}
+	
+	
+	
+	
+	public static void main(String[] args)
+	{
+		//System.out.println(LoadStopWords());
+		//System.out.println(LoadFile());
+		GenarateFile();
+	}
+}
+	
+
